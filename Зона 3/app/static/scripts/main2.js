0f4462a3cd56364a0b4c283a6829a3ae524c5dcd
s-nosonov-chernomor <s.nosonov@gmail.com>
2025-08-08 17:45:47 +0300

    Initial commit

--- Зона 3/app/static/scripts/main2.js
+++ Зона 3/app/static/scripts/main2.js
@@ -1,82 +1,14 @@
 const buttonTexts = {
-    'Клапан 1.1':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.2':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.3':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.4':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.5':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.6':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.7':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.8':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.9':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 1.10':          { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.1':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.2':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.3':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.4':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.5':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.6':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.7':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.8':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.9':           { on: 'Включен',   off: 'Выключен' },
-    'Клапан 2.10':          { on: 'Включен',   off: 'Выключен' },
-    'Клапан перемешивания': { on: 'Включен',   off: 'Выключен' },
-    'Наполнение бака':      { on: 'Включен',   off: 'Выключен' },
-    'Слив с бака':          { on: 'Включен',   off: 'Выключен' },
+
+    'Перемешивание': { on: 'Включен',   off: 'Выключен' },
     'Насос':                { on: 'Включен',   off: 'Выключен' },
-    'Освещение 1 этаж':     { on: 'Включен',   off: 'Выключен' },
-    'Освещение 2 этаж':     { on: 'Включен',   off: 'Выключен' },
+    'Свет':     { on: 'Включен',   off: 'Выключен' },
+    'Полив':     { on: 'Включен',   off: 'Выключен' },
     'Режим эксплуатации': { on: 'Включен', off: 'Выключен' },
     'Растворный узел': { on: 'Включен', off: 'Выключен' },
 
-    'КАНАЛ Линия 1 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 1 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 1 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 1 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 2 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 2 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 2 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 2 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 3 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 3 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 3 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 3 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 4 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 4 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 4 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 4 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 5 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 5 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 5 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 5 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 6 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 6 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 6 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 6 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 7 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 7 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 7 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 7 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 8 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 8 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 8 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 8 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 9 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 9 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 9 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 9 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
-
-    'КАНАЛ Линия 10 этаж 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 10 этаж 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
-    'КАНАЛ Линия 10 этаж 2 БЕЛЫЙ':  { on: 'on', off: 'off' },
-    'КАНАЛ Линия 10 этаж 2 КРАСНЫЙ':{ on: 'on', off: 'off' },
+    'КАНАЛ 1 БЕЛЫЙ':  { on: 'on', off: 'off' },
+    'КАНАЛ 1 КРАСНЫЙ':{ on: 'on', off: 'off' },
 
 };
 
@@ -160,160 +92,36 @@ async function updateData() {
                 }
             }
         };
-        updateButton('shelf1',  'Клапан 1.1');
-        updateButton('shelf2',  'Клапан 1.2');
-        updateButton('shelf3',  'Клапан 1.3');
-        updateButton('shelf4',  'Клапан 1.4');
-        updateButton('shelf5',  'Клапан 1.5');
-        updateButton('shelf6',  'Клапан 1.6');
-        updateButton('shelf7',  'Клапан 1.7');
-        updateButton('shelf8',  'Клапан 1.8');
-        updateButton('shelf9',  'Клапан 1.9');
-        updateButton('shelf10', 'Клапан 1.10');
 
-        updateButton('shelf11', 'Клапан 2.1');
-        updateButton('shelf12', 'Клапан 2.2');
-        updateButton('shelf13', 'Клапан 2.3');
-        updateButton('shelf14', 'Клапан 2.4');
-        updateButton('shelf15', 'Клапан 2.5');
-        updateButton('shelf16', 'Клапан 2.6');
-        updateButton('shelf17', 'Клапан 2.7');
-        updateButton('shelf18', 'Клапан 2.8');
-        updateButton('shelf19', 'Клапан 2.9');
-        updateButton('shelf20', 'Клапан 2.10');
-
-        updateButton('shelf21', 'Клапан перемешивания');
-
-        updateButton('shelf25', 'Наполнение бака');
-        updateButton('shelf26', 'Слив с бака');
-
-        updateButton('shelf28', 'Насос');
-        updateButton('shelf29', 'Освещение 1 этаж');
-        updateButton('shelf30', 'Освещение 2 этаж');
+        updateButton('nasos', 'Насос');
+        updateButton('peremesh', 'Перемешивание');
+        updateButton('poliv', 'Полив');
+        updateButton('svet', 'Свет');
 
         updateButton('mode_param', 'Режим эксплуатации');
         updateButton('mixing', 'Растворный узел');
 
-        updateButton2('chanel_1_1_white', 'КАНАЛ Линия 1 этаж 1 БЕЛЫЙ');
-        updateText('level_1_1_white',    'ЯРКОСТЬ Линия 1 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_1_1_red',   'КАНАЛ Линия 1 этаж 1 КРАСНЫЙ');
-        updateText('level_1_1_red',      'ЯРКОСТЬ Линия 1 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_1_2_white', 'КАНАЛ Линия 1 этаж 2 БЕЛЫЙ');
-        updateText('level_1_2_white',    'ЯРКОСТЬ Линия 1 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_1_2_red',   'КАНАЛ Линия 1 этаж 2 КРАСНЫЙ');
-        updateText('level_1_2_red',      'ЯРКОСТЬ Линия 1 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_2_1_white', 'КАНАЛ Линия 2 этаж 1 БЕЛЫЙ');
-        updateText('level_2_1_white',    'ЯРКОСТЬ Линия 2 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_2_1_red',   'КАНАЛ Линия 2 этаж 1 КРАСНЫЙ');
-        updateText('level_2_1_red',      'ЯРКОСТЬ Линия 2 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_2_2_white', 'КАНАЛ Линия 2 этаж 2 БЕЛЫЙ');
-        updateText('level_2_2_white',    'ЯРКОСТЬ Линия 2 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_2_2_red',   'КАНАЛ Линия 2 этаж 2 КРАСНЫЙ');
-        updateText('level_2_2_red',      'ЯРКОСТЬ Линия 2 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_3_1_white', 'КАНАЛ Линия 3 этаж 1 БЕЛЫЙ');
-        updateText('level_3_1_white',    'ЯРКОСТЬ Линия 3 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_3_1_red',   'КАНАЛ Линия 3 этаж 1 КРАСНЫЙ');
-        updateText('level_3_1_red',      'ЯРКОСТЬ Линия 3 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_3_2_white', 'КАНАЛ Линия 3 этаж 2 БЕЛЫЙ');
-        updateText('level_3_2_white',    'ЯРКОСТЬ Линия 3 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_3_2_red',   'КАНАЛ Линия 3 этаж 2 КРАСНЫЙ');
-        updateText('level_3_2_red',      'ЯРКОСТЬ Линия 3 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_4_1_white', 'КАНАЛ Линия 4 этаж 1 БЕЛЫЙ');
-        updateText('level_4_1_white',    'ЯРКОСТЬ Линия 4 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_4_1_red',   'КАНАЛ Линия 4 этаж 1 КРАСНЫЙ');
-        updateText('level_4_1_red',      'ЯРКОСТЬ Линия 4 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_4_2_white', 'КАНАЛ Линия 4 этаж 2 БЕЛЫЙ');
-        updateText('level_4_2_white',    'ЯРКОСТЬ Линия 4 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_4_2_red',   'КАНАЛ Линия 4 этаж 2 КРАСНЫЙ');
-        updateText('level_4_2_red',      'ЯРКОСТЬ Линия 4 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_5_1_white', 'КАНАЛ Линия 5 этаж 1 БЕЛЫЙ');
-        updateText('level_5_1_white',    'ЯРКОСТЬ Линия 5 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_5_1_red',   'КАНАЛ Линия 5 этаж 1 КРАСНЫЙ');
-        updateText('level_5_1_red',      'ЯРКОСТЬ Линия 5 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_5_2_white', 'КАНАЛ Линия 5 этаж 2 БЕЛЫЙ');
-        updateText('level_5_2_white',    'ЯРКОСТЬ Линия 5 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_5_2_red',   'КАНАЛ Линия 5 этаж 2 КРАСНЫЙ');
-        updateText('level_5_2_red',      'ЯРКОСТЬ Линия 5 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_6_1_white', 'КАНАЛ Линия 6 этаж 1 БЕЛЫЙ');
-        updateText('level_6_1_white',    'ЯРКОСТЬ Линия 6 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_6_1_red',   'КАНАЛ Линия 6 этаж 1 КРАСНЫЙ');
-        updateText('level_6_1_red',      'ЯРКОСТЬ Линия 6 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_6_2_white', 'КАНАЛ Линия 6 этаж 2 БЕЛЫЙ');
-        updateText('level_6_2_white',    'ЯРКОСТЬ Линия 6 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_6_2_red',   'КАНАЛ Линия 6 этаж 2 КРАСНЫЙ');
-        updateText('level_6_2_red',      'ЯРКОСТЬ Линия 6 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_7_1_white', 'КАНАЛ Линия 7 этаж 1 БЕЛЫЙ');
-        updateText('level_7_1_white',    'ЯРКОСТЬ Линия 7 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_7_1_red',   'КАНАЛ Линия 7 этаж 1 КРАСНЫЙ');
-        updateText('level_7_1_red',      'ЯРКОСТЬ Линия 7 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_7_2_white', 'КАНАЛ Линия 7 этаж 2 БЕЛЫЙ');
-        updateText('level_7_2_white',    'ЯРКОСТЬ Линия 7 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_7_2_red',   'КАНАЛ Линия 7 этаж 2 КРАСНЫЙ');
-        updateText('level_7_2_red',      'ЯРКОСТЬ Линия 7 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_8_1_white', 'КАНАЛ Линия 8 этаж 1 БЕЛЫЙ');
-        updateText('level_8_1_white',    'ЯРКОСТЬ Линия 8 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_8_1_red',   'КАНАЛ Линия 8 этаж 1 КРАСНЫЙ');
-        updateText('level_8_1_red',      'ЯРКОСТЬ Линия 8 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_8_2_white', 'КАНАЛ Линия 8 этаж 2 БЕЛЫЙ');
-        updateText('level_8_2_white',    'ЯРКОСТЬ Линия 8 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_8_2_red',   'КАНАЛ Линия 8 этаж 2 КРАСНЫЙ');
-        updateText('level_8_2_red',      'ЯРКОСТЬ Линия 8 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_9_1_white', 'КАНАЛ Линия 9 этаж 1 БЕЛЫЙ');
-        updateText('level_9_1_white',    'ЯРКОСТЬ Линия 9 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_9_1_red',   'КАНАЛ Линия 9 этаж 1 КРАСНЫЙ');
-        updateText('level_9_1_red',      'ЯРКОСТЬ Линия 9 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_9_2_white', 'КАНАЛ Линия 9 этаж 2 БЕЛЫЙ');
-        updateText('level_9_2_white',    'ЯРКОСТЬ Линия 9 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_9_2_red',   'КАНАЛ Линия 9 этаж 2 КРАСНЫЙ');
-        updateText('level_9_2_red',      'ЯРКОСТЬ Линия 9 этаж 2 КРАСНЫЙ');
-
-        updateButton2('chanel_10_1_white', 'КАНАЛ Линия 10 этаж 1 БЕЛЫЙ');
-        updateText('level_10_1_white',    'ЯРКОСТЬ Линия 10 этаж 1 БЕЛЫЙ');
-        updateButton2('chanel_10_1_red',   'КАНАЛ Линия 10 этаж 1 КРАСНЫЙ');
-        updateText('level_10_1_red',      'ЯРКОСТЬ Линия 10 этаж 1 КРАСНЫЙ');
-
-        updateButton2('chanel_10_2_white', 'КАНАЛ Линия 10 этаж 2 БЕЛЫЙ');
-        updateText('level_10_2_white',    'ЯРКОСТЬ Линия 10 этаж 2 БЕЛЫЙ');
-        updateButton2('chanel_10_2_red',   'КАНАЛ Линия 10 этаж 2 КРАСНЫЙ');
-        updateText('level_10_2_red',      'ЯРКОСТЬ Линия 10 этаж 2 КРАСНЫЙ');
+        updateButton2('chanel_1_1_white', 'КАНАЛ 1 БЕЛЫЙ');
+        updateText('level_1_1_white',    'ЯРКОСТЬ 1 БЕЛЫЙ');
+        updateButton2('chanel_1_1_red',   'КАНАЛ 1 КРАСНЫЙ');
+        updateText('level_1_1_red',      'ЯРКОСТЬ 1 КРАСНЫЙ');
 
         updateText2('UNIT_ID_PH', 'Уровень PH');
         updateText2('UNIT_ID_EC', 'Уровень EC');
 
         // Обновляем графические индикаторы для основного бака
-        updateBinaryIndicator('indicator-level-1', 'Бак уровень 92%');
-        updateBinaryIndicator('indicator-level-2', 'Бак уровень 75%');
-        updateBinaryIndicator('indicator-level-3', 'Бак уровень 57%');
-        updateBinaryIndicator('indicator-level-4', 'Бак уровень 40%');
-        updateBinaryIndicator('indicator-level-5', 'Бак уровень 22%');
-        updateBinaryIndicator('indicator-level-6', 'Бак уровень минимум 5%');
+        updateBinaryIndicator('indicator-level-1', 'Уровень 3');
+        updateBinaryIndicator('indicator-level-2', 'Уровень 2');
+        updateBinaryIndicator('indicator-level-3', 'Уровень 1 минимум');
+
 
         // Обновляем графические индикаторы для баков компонентов
-        updateBinaryIndicator('indicator-level-A', 'Середина компонент А');
-        updateBinaryIndicator('indicator-level-A2', 'Минимум компонент А');
-        updateBinaryIndicator('indicator-level-B', 'Середина компонент В');
-        updateBinaryIndicator('indicator-level-B2', 'Минимум компонент В');
-        updateBinaryIndicator('indicator-level-K', 'Середина кислота');
-        updateBinaryIndicator('indicator-level-K2', 'Минимум кислота');
 
-        updateStatusText('statusA', 'Подача A в бак');
+        updateBinaryIndicator('indicator-level-A2', 'Уровень А');
+        updateBinaryIndicator('indicator-level-B2', 'Уровень В');
+        updateBinaryIndicator('indicator-level-K2', 'Уровень К');
+
+        updateStatusText('statusA', 'Подача А в бак');
         updateStatusText('statusB', 'Подача В в бак');
         updateStatusText('statusK', 'Подача кислоты в бак');
         updateTimeText('timeA', 'Время подачи A в бак');
